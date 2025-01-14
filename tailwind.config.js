de3a4d8a96a6d895261343932226a3926adab51f
Siam-66 <siammahmud66@gmail.com>
2025-01-14 18:38:06 +0600

    setuping the project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+import daisyui from 'daisyui';
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        glow: {
+          "0%, 100%": { textShadow: "0 0 10px #fbbf24, 0 0 20px #fbbf24, 0 0 30px #f59e0b" },
+          "50%": { textShadow: "0 0 20px #fbbf24, 0 0 40px #fbbf24, 0 0 60px #f59e0b" },
+        },
+      },
+      animation: {
+        glow: "glow 2s infinite",
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+  themes: ["light"],
+  },
+}
